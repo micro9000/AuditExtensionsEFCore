cead7655ea3e12f2f98a30cf422946e77b6938bd
Raniel Garcia <20143627+micro9000@users.noreply.github.com>
2023-04-04 14:17:34 +0800

    Create ManyToManyEntityAttribute.cs

---
+++ ManyToManyEntityAttribute.cs
@@ -0,0 +1,14 @@
+using System;
+
+[AttributeUsage(validOn: AttributeTargets.Class, AllowMultiple = false)]
+public class ManyToManyEntityAttribute : Attribute
+{
+  public string FirstKey {get; }
+  public string SecondKey {get;}
+  
+  public ManyToManyEntityAttribute(string firstKey, string secondKey)
+  {
+    FirstKey = firstKey;
+    SecondKey = secondKey;
+  }
+}
