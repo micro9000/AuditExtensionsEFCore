454b992fe6f13e9efaedac7c7822a90873054994
Raniel Garcia <20143627+micro9000@users.noreply.github.com>
2023-04-04 13:54:01 +0800

    Create DatabaseContext.cs

---
+++ DatabaseContext.cs
@@ -0,0 +1,31 @@
+public class DatabaseContext : DbContext
+{
+   private readonly ICurrentUserAccessor _currentUserAccessor;
+   
+   public DatabaseContext (DbContextOptions options, ICurrentUserAccessor currentUserAccessor) : base(options)
+   {
+    _currentUserAccessor = currentUserAccessor;
+   }
+   
+   public DbSet<Audit> AuditRecords => Set<Audit>();
+   
+   public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+   {
+      InterceptChanges();
+      return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+   }
+   
+   public override int SaveChanges (bool acceptAllChangesOnSuccess)
+   {
+      InterceptChanges();
+      return base.SaveChanges(acceptAllChangesOnSuccess);
+   }
+   
+   private void InterceptChanges()
+   {
+      // Orders matters here as the soft delete override changes Deleted records to Modified
+      var user = _currentUserAccessor.GetCurrentUser();
+      AuditRecords.AddRange(ChangeTracker.GenerateAuditRecords(user));
+      ChangeTracker.ApplySoftDeleteOverride();
+   }
+}
